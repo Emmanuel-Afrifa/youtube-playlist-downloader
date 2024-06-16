ced34d40261004da2097d2c6cf4feaf03390aedf
Emmanuel-Afrifa <emmaquame9@gmail.com>
2024-06-16 23:50:43 +0000

    I updated the ersion 2 file by rewriting the program using OOP

--- YPD.spec
+++ YPD.spec
@@ -5,7 +5,7 @@ a = Analysis(
     ['src\\main.py'],
     pathex=[],
     binaries=[],
-    datas=[],
+    datas=[('C:\\Users\\emmaq\\OneDrive\\Desktop\\auto_playlist_installer_library\\src\\youtube_logo.png', '.')],
     hiddenimports=[],
     hookspath=[],
     hooksconfig={},
@@ -35,5 +35,5 @@ exe = EXE(
     target_arch=None,
     codesign_identity=None,
     entitlements_file=None,
-    icon=['images\\YPD.ico'],
+    icon=['YPD.ico'],
 )
--- src/auto_playlist_downloader_v2.py
+++ src/auto_playlist_downloader_v2.py
@@ -2,119 +2,132 @@
 import os
 from pytube import Playlist
 import queue
+import sys
 import threading
 import tkinter as tk
 from tkinter import ttk
 
 
-def YPD():
-    """
-    Returns the GUI version of the YPD application
-    """
-    root = tk.Tk()
-    root.title('Youtube Playlist Downloader')
-    root.configure(bg='#A9A9A9')
-    root.geometry('600x480')
-    icon_pic = tk.PhotoImage(file="../images/youtube_logo.png")
-    root.iconphoto(False, icon_pic)
+def image_path(image_relative_path):
+    base_path = getattr(sys, '_MEIPASS', os.path.dirname(os.path.abspath(__file__)))
+    return os.path.join(base_path, image_relative_path)
 
-    root.resizable(height=False, width=False)
+class YPD:
+    def __init__(self):
+        self.root = tk.Tk()
+        self.root.title('Youtube Playlist Downloader')
+        self.root.configure(bg='#A9A9A9')
+        self.root.geometry('600x480')
+        self.img_path = image_path('youtube_logo.png')
+        self.icon_pic = tk.PhotoImage(file=self.img_path)
+        self.root.iconphoto(False, self.icon_pic)
 
-    widgets_list = []
+        self.root.resizable(height=False, width=False)
 
-    # NEW DIRECTORY INFO
-    label_new_path = tk.Label(root, text='Enter the name of the new folder for the playlist', bd=8, font=('calibre', 12, 'bold'), height=1, bg='#A9A9A9', foreground='white')
-    label_new_path.pack(pady=19)
-    widgets_list.append(label_new_path)
+        self.widgets_list = []
 
-    new_path_str = tk.StringVar()
-    entry_new_path= tk.Entry(root, textvariable=new_path_str, selectbackground='blue', selectforeground='white', width=90)
-    entry_new_path.pack(padx=15)
-    widgets_list.append(entry_new_path)
+        # NEW DIRECTORY INFO
+        self.label_new_path = tk.Label(self.root, text='Enter the name of the new folder for the playlist', bd=8, font=('calibre', 12, 'bold'), height=1, bg='#A9A9A9', foreground='white')
+        self.label_new_path.pack(pady=19)
+        self.widgets_list.append(self.label_new_path)
+
+        self.new_path_str = tk.StringVar()
+        self.entry_new_path= tk.Entry(self.root, textvariable=self.new_path_str, selectbackground='blue', selectforeground='white', width=90)
+        self.entry_new_path.pack(padx=15)
+        self.widgets_list.append(self.entry_new_path)
 
 
-    # URL INFO
-    label_url = tk.Label(root, text='Enter the URL of the playlist', bd=8, font=('calibre', 12, 'bold'), height=1, bg='#A9A9A9', foreground='white')
-    label_url.pack(pady=19)
-    widgets_list.append(label_url)
+        # URL INFO
+        self.label_url = tk.Label(self.root, text='Enter the URL of the playlist', bd=8, font=('calibre', 12, 'bold'), height=1, bg='#A9A9A9', foreground='white')
+        self.label_url.pack(pady=19)
+        self.widgets_list.append(self.label_url)
 
-    url_str = tk.StringVar()
-    entry_url = tk.Entry(root, textvariable=url_str, selectbackground='blue', selectforeground='white', width=90)
-    entry_url.pack(padx=15)
-    widgets_list.append(entry_url)
+        self.url_str = tk.StringVar()
+        self.entry_url = tk.Entry(self.root, textvariable=self.url_str, selectbackground='blue', selectforeground='white', width=90)
+        self.entry_url.pack(padx=15)
+        self.widgets_list.append(self.entry_url)
 
-    def destroy_widgets_after_download():
-        if len(widgets_list) > 0:
-            download_index = widgets_list.index(download_button)
-            for widget in widgets_list[download_index+1:]:
+        self.download_log = tk.Label(self.root, text='Number of files downloaded: \n\n0', width=75, highlightthickness=5, highlightbackground='red', height=5)
+        self.download_log.pack(pady=18)
+        self.widgets_list.append(self.download_log)
+
+        self.message_queue = queue.Queue()
+        self.stop_download = tk.BooleanVar(value=False)
+
+        self.download_button = tk.Button(self.root, text='Download Playlist', bg='#A9A9A9', foreground='white', activebackground='white', \
+                        activeforeground='#A9A9A9', relief=tk.RAISED, width=28, height=1, font=('calibre', 12, 'bold'), cursor='arrow',
+                        command=self.get_playlist)
+        self.download_button.pack(pady=12)
+        self.widgets_list.append(self.download_button)
+
+        self.root.mainloop()
+
+    def destroy_widgets_after_download(self):
+        if len(self.widgets_list) > 0:
+            download_index = self.widgets_list.index(self.download_button)
+            for widget in self.widgets_list[download_index+1:]:
                 widget.destroy()
 
-    download_log = tk.Label(root, text='Number of files downloaded: \n\n0', width=75, highlightthickness=5, highlightbackground='red', height=5)
-    download_log.pack(pady=18)
-    widgets_list.append(download_log)
 
-    message_queue = queue.Queue()
-    stop_download = tk.BooleanVar(value=False)
-
-    def download_videos(playlist, stop_button):
+    def download_videos(self, playlist, stop_button):
         global stop_download
         for i in range(len(playlist.videos)):
-            if stop_download.get():
+            if self.stop_download.get():
                 break
             
             message = f"Number of files downloaded: \n\n{i + 1}"
-            message_queue.put(message)
-            root.after(0, label_updater)
+            self.message_queue.put(message)
+            self.root.after(0, self.label_updater)
             playlist.videos[i].streams.get_highest_resolution().download()
-        root.after(0, lambda: download_button.config(state='normal'))
-        root.after(0, lambda: stop_button.pack_forget())
-        stop_download.set(False)
+        self.root.after(0, lambda: self.download_button.config(state='normal'))
+        self.root.after(0, lambda: stop_button.pack_forget())
+        self.stop_download.set(False)
 
             
             
-    def stop_downloading(stop_button):
-        stop_download.set(True)
+    def stop_downloading(self, stop_button):
+        self.stop_download.set(True)
         stop_button.pack_forget()
         
 
-    def label_updater():
+    def label_updater(self):
         try:
-            while not message_queue.empty():
-                new_message = message_queue.get_nowait()
-                download_log.config(text=new_message)
+            while not self.message_queue.empty():
+                new_message = self.message_queue.get_nowait()
+                self.download_log.config(text=new_message)
         except queue.Empty:
             pass
-        root.after(10, label_updater)
+        self.root.after(10, self.label_updater)
 
-    def get_playlist():
-        download_button.config(state='disabled')
-        url = url_str.get()
+    def get_playlist(self):
+        self.download_button.config(state='disabled')
+        url = self.url_str.get()
 
-        destroy_widgets_after_download()
+        self.destroy_widgets_after_download()
 
         try:
-            url = url_str.get()
+            url = self.url_str.get()
             if Playlist(url):
                 playlist = Playlist(url)
             else:
                 print('boooo')
 
-                warning_label = tk.Label(root, text='Kindly fill the necessary fields with the correct details!', height=5, anchor=tk.CENTER, \
+                warning_label = tk.Label(self.root, text='Kindly fill the necessary fields with the correct details!', height=5, anchor=tk.CENTER, \
                                     font=('calibre', 16, 'bold'), fg='red', bg='#A9A9A9')
                 warning_label.pack(pady=10)
-                widgets_list.append(warning_label)
+                self.widgets_list.append(warning_label)
                 return
 
             # global stop_button
-            stop_button = tk.Button(root, text='STOP', bg='red', foreground='white', activebackground='white', \
+            stop_button = tk.Button(self.root, text='STOP', bg='red', foreground='white', activebackground='white', \
                             activeforeground='red', relief=tk.RAISED, width=20, height=1, font=('calibre', 12, 'bold'), cursor='arrow', \
-                                command=lambda: stop_downloading(stop_button))
+                                command=lambda: self.stop_downloading(stop_button))
             stop_button.pack()
-            widgets_list.append(stop_button)
+            self.widgets_list.append(stop_button)
 
 
             current_path = os.getcwd()
-            new_path = current_path + '\\' + new_path_str.get()
+            new_path = current_path + '\\' + self.new_path_str.get()
             try:
                 os.makedirs(new_path)
             except:
@@ -124,22 +137,15 @@ def YPD():
             os.chdir(new_path)
 
             # downloading the videos in the playlist
-            download_thread = threading.Thread(target=download_videos, args=(playlist,stop_button))
+            download_thread = threading.Thread(target=self.download_videos, args=(playlist,stop_button))
             download_thread.start()
-            label_updater()
+            self.label_updater()
 
         except:
-            warning_label = tk.Label(root, text='Kindly fill the necessary fields with the correct details!', height=5, anchor=tk.CENTER, \
+            warning_label = tk.Label(self.root, text='Kindly fill the necessary fields with the correct details!', height=5, anchor=tk.CENTER, \
                                     font=('calibre', 16, 'bold'), fg='red', bg='#A9A9A9')
             warning_label.pack(pady=10)
-            widgets_list.append(warning_label)
-            download_button.config(state='active')
+            self.widgets_list.append(warning_label)
+            self.download_button.config(state='active')
 
-    root.after(10, label_updater)
-
-    download_button = tk.Button(root, text='Download Playlist', bg='#A9A9A9', foreground='white', activebackground='white', \
-                        activeforeground='#A9A9A9', relief=tk.RAISED, width=28, height=1, font=('calibre', 12, 'bold'), cursor='arrow',
-                        command=get_playlist)
-    download_button.pack(pady=12)
-    widgets_list.append(download_button)
-    root.mainloop()
+        self.root.after(10, self.label_updater)
